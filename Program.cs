d461cd020cd6e3e7a71629e6bb23e487eb09ea1b
Juan Rodas <jrodasc>
2018-11-22 12:34:54 -0600

    Programa inicial

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace apirest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetRequest("http://www.google.com.mx");
+            PostRequest("http://ptsv2.com/t/m9v0g-1542909408/post");
+            Console.ReadKey();
+        }
+
+        async static void GetRequest(string url)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                using(HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using(HttpContent content = response.Content)
+                    {
+                        //string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+                        Console.WriteLine(headers);
+                    }
+                }
+            }
+        }
+        async static void PostRequest(string url)
+        {
+            IEnumerable<KeyValuePair<string,string> > queries = new List<KeyValuePair<string,string>>()
+            {
+                new KeyValuePair<string, string>("query1","jama2"),
+                new KeyValuePair<string, string>("query1","jrodas")
+            };
+            HttpContent q = new FormUrlEncodedContent(queries);
+            using(HttpClient client = new HttpClient())
+            {
+                using(HttpResponseMessage response = await client.PostAsync(url,q))
+                {
+                    using(HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+                        Console.WriteLine(mycontent);
+                    }
+                }
+            }
+        }
+    }
+}
